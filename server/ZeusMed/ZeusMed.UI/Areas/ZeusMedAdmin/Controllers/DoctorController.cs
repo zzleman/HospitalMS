50417f560bcaacb82c5fd5668eae111db1d3d884
Leman <leman@Leman-Air.mshome.net>
2023-09-20 14:25:06 +0400

    admin panel's layout is ready

---
+++ server/ZeusMed/ZeusMed.UI/Areas/ZeusMedAdmin/Controllers/DoctorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZeusMed.UI.Areas.ZeusMedAdmin.Controllers;
+[Area("ZeusMedAdmin")]
+public class DoctorController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
+
