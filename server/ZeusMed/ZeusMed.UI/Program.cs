2c080e39148faa6830b9efa4afc9e77a1a979a66
Leman <leman@Leman-MacBook-Air.local>
2023-09-23 09:30:26 +0400

    error page is added

--- server/ZeusMed/ZeusMed.UI/Program.cs
+++ server/ZeusMed/ZeusMed.UI/Program.cs
@@ -11,6 +11,22 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 var app = builder.Build();
 app.UseStaticFiles();
 
+
+if (app.Environment.IsDevelopment())
+{
+    DeveloperExceptionPageOptions developerExceptionPageOptions = new();
+    developerExceptionPageOptions.SourceCodeLineCount = 1;
+    app.UseDeveloperExceptionPage(developerExceptionPageOptions);
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
+app.UseStatusCodePagesWithRedirects("/StatusCodeError/{0}");
+
+
+
 app.MapControllerRoute(
     name: "areas",
     pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{Id?}"
