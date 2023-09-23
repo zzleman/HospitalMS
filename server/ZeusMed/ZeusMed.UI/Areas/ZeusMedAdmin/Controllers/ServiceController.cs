0938ae0be4c20f74ce85aece7995fc63805fa072
Leman <leman@Leman-MacBook-Air.local>
2023-09-23 16:35:55 +0400

    service index is added

---
+++ server/ZeusMed/ZeusMed.UI/Areas/ZeusMedAdmin/Controllers/ServiceController.cs
@@ -0,0 +1,28 @@
+﻿
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ZeusMed.Core.Entities;
+using ZeusMed.DataAccess.Contexts;
+using ZeusMed.UI.Areas.ZeusMedAdmin.ViewModels.ServiceViewModel;
+
+namespace ZeusMed.UI.Areas.ZeusMedAdmin.Controllers;
+[Area("ZeusMedAdmin")]
+public class ServiceController : Controller
+{
+    private readonly AppDbContext _context;
+    private readonly IMapper _mapper;
+
+    public ServiceController(AppDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.Doctors.ToListAsync());
+    }
+
+}
+
---
+++ server/ZeusMed/ZeusMed.UI/Areas/ZeusMedAdmin/ViewModels/ServiceViewModel/ServicePostVM.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace ZeusMed.UI.Areas.ZeusMedAdmin.ViewModels.ServiceViewModel;
+
+public class ServicePostVM
+{
+    [Required, StringLength(40)]
+    public string? Title { get; set; } = null!;
+
+    [Required, StringLength(200)]
+    public string? Description { get; set; } = null!;
+
+    [Required, StringLength(255)]
+    public string? ImagePath { get; set; } = null!;
+}
+
